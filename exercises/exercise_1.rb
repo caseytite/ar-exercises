8748bd13f5d6b32fdeeab21b2a416db65ab79656
Casey <caseyjtite@gmail.com>
2022-03-07 17:43:10 -0800

    complete exercise two

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,20 +1,20 @@
 require_relative '../setup'
 
-# 1. Use Active Record's `create` class method multiple times to create 3 stores in the database:
-#   * Burnaby (annual_revenue of 300000, carries men's and women's apparel)
-#   * Richmond (annual_revenue of 1260000 carries women's apparel only)
-#   * Gastown (annual_revenue of 190000 carries men's apparel only)
-# 2. Output (`puts`) the number of the stores using ActiveRecord's `count` method, to ensure that there are three stores in the database.
 
 puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
 
+# 1. Use Active Record's `create` class method multiple times to create 3 stores in the database:
+#   * Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+#   * Richmond (annual_revenue of 1260000 carries women's apparel only)
+#   * Gastown (annual_revenue of 190000 carries men's apparel only)
 store = Store.create(name: "Burnaby", annual_revenue: "300000", womens_apparel: true, mens_apparel:true)
 store = Store.create(name: "Richmond", annual_revenue: "1260000", womens_apparel: true, mens_apparel:false)
 store = Store.create(name: "Gastown", annual_revenue: "190000", womens_apparel: false, mens_apparel:true)
 
+# 2. Output (`puts`) the number of the stores using ActiveRecord's `count` method, to ensure that there are three stores in the database.
 number_of_stores = Store.count
 puts number_of_stores
 
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,9 +1,6 @@
 require_relative '../setup'
 require_relative './exercise_1'
 
-# 1. Load the first store (with `id = 1`) from the database and assign it to an instance variable `@store1`.
-# 2. Load the second store from the database and assign it to `@store2`.
-# 3. Update the first store (`@store1`) instance in the database. (Change its name or something.)
 
 
 puts "Exercise 2"
@@ -12,8 +9,10 @@ puts "----------"
 # Your code goes here ...
 
 
+# 1. Load the first store (with `id = 1`) from the database and assign it to an instance variable `@store1`.
 @store1 = Store.find_by(id:1)
+# 2. Load the second store from the database and assign it to `@store2`.
 @store2 = Store.find_by(id:2)
-
+# 3. Update the first store (`@store1`) instance in the database. (Change its name or something.)
 @store1.name = "Victoria"
 @store1.save
