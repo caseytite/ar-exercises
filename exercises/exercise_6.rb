6acf01ffb28370c85fcc7919fe9bd4a03e51da1c
Casey <caseyjtite@gmail.com>
2022-03-07 22:00:43 -0800

    finish exercise 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -21,7 +21,7 @@ puts "----------"
 # 3. Add some data into employees. Here's an example of one (note how it differs from how you create stores): `@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)`
 @store1.employees.create(first_name: "Casey", last_name: "T", hourly_rate: 60)
 @store1.employees.create(first_name: "Allison", last_name: "G", hourly_rate: 100)
-@store1.employees.create(first_name: "Owen", last_name: "GT", hourly_rate: 250)
+@store1.employees.create(first_name: "Owen", last_name: "GT", hourly_rate: 150)
 
 # 4. Go ahead and create some more employees using the create method. You can do this by making multiple calls to create (like you have before.) No need to assign the employees to variables though. Create them through the `@store#` instance variables that you defined in previous exercises. Create a bunch under `@store1` (Burnaby) and `@store2` (Richmond). Eg: `@store1.employees.create(...)`.
 
@@ -31,16 +31,16 @@ puts "----------"
 
 @store2.employees.create(first_name: "Socks", last_name: "T", hourly_rate: 60)
 @store2.employees.create(first_name: "Mittens", last_name: "G", hourly_rate: 100)
-@store2.employees.create(first_name: "Fluffykins", last_name: "GT", hourly_rate: 250)
+@store2.employees.create(first_name: "Fluffykins", last_name: "GT", hourly_rate: 200)
 
 @store4.employees.create(first_name: "Oscar", last_name: "T", hourly_rate: 60)
 @store4.employees.create(first_name: "Hobbes", last_name: "G", hourly_rate: 100)
-@store4.employees.create(first_name: "Freddy", last_name: "GT", hourly_rate: 250)
+@store4.employees.create(first_name: "Freddy", last_name: "GT", hourly_rate: 150)
 
 @store5.employees.create(first_name: "Bandit", last_name: "T", hourly_rate: 60)
 @store5.employees.create(first_name: "Daisy", last_name: "G", hourly_rate: 100)
-@store5.employees.create(first_name: "Huck", last_name: "GT", hourly_rate: 250)
+@store5.employees.create(first_name: "Huck", last_name: "GT", hourly_rate: 150)
 
 @store6.employees.create(first_name: "Daniel", last_name: "T", hourly_rate: 60)
 @store6.employees.create(first_name: "Jessie", last_name: "G", hourly_rate: 100)
-@store6.employees.create(first_name: "Wyatt", last_name: "GT", hourly_rate: 250)
+@store6.employees.create(first_name: "Wyatt", last_name: "GT", hourly_rate: 150)
